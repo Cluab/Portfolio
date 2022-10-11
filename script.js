d6e4f973566aceb141f585b9d6b819de2d7fc867
cluab <cluab11222@gmail.com>
2022-10-11 11:35:35 +0200

    updated javasecipt section

--- script.js
+++ script.js
@@ -13,5 +13,5 @@ hamburger.addEventListener('click', () => {
 document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
-  logo.classList.toggle('active');
+  logo.classList.remove('active');
 }));
