49213e193203760be5e6d891db7ff57e4b752550
cluab <cluab11222@gmail.com>
2022-10-11 20:08:24 +0200

    fixed mobile styling

--- script.js
+++ script.js
@@ -40,10 +40,13 @@ popup.innerHTML = `
    <div class ="desktop-popup">
    <img class='popup-image' src="/img/Snapshoot Portfolio.png" alt="popup work preview">
    <div class="popup-end">
-   <p class="lorem">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+   <p class="lorem-1">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
     when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.
      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
    when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.</p>
+   <p class="lorem">Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+   Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+    but also the leap into electronic typesetting, remaining essent</p>
     <div class="popuplinks"><button class="live">See Live<img src="/img/Icon.png" alt="icon-image"></button>
     <button class="live">See Source<img src="/img/Vector.png" alt="github-icon"></button></div>
     </div>
