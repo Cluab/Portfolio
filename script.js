0e1ebe13ee84b9bb8548bcea543f92b2a760e6cc
cluab <cluab11222@gmail.com>
2022-10-12 15:03:49 +0200

    added linters errors

--- script.js
+++ script.js
@@ -18,16 +18,28 @@ document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click'
   logo.classList.remove('active');
 }));
 
+const workcardpop = [
+  {
+    id: 0,
+    name: ['Multi-Post Stories', 'Keeping track of hundreds  of components website'],
+    description:
+           ['Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,but also the leap into electronic typesetting, remaining essent',
+             "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita."],
+    Fimage: 'img/Snapshoot Portfolio.png',
+    technologies: ['html', 'bootstrap', 'Ruby'],
+    Llive: 'https://cluab.github.io/Portfolio/',
+    Lsource: 'https://github.com/Cluab/Portfolio',
+  }];
 
-function popup(){
-  workcardpop.forEach( (workcardpop) => {
-project.addEventListener('click', () => {
-  const main = document.createElement('div');
-  main.className = 'main-popup';
-  const popup = document.createElement('div');
-  popup.className = 'popup';
+function popup() {
+  workcardpop.forEach((workcardpop) => {
+    project.addEventListener('click', () => {
+      const main = document.createElement('div');
+      main.className = 'main-popup';
+      const popup = document.createElement('div');
+      popup.className = 'popup';
 
-popup.innerHTML = `
+      popup.innerHTML = `
 <div class="popup-menu">
   <div class="popgap">
         <div class="popup-header">
@@ -56,23 +68,18 @@ popup.innerHTML = `
     </div></div>      
 `;
 
-  main.appendChild(popup);
-  body.appendChild(main);
+      main.appendChild(popup);
+      body.appendChild(main);
 
-  const close = document.querySelector('.close-button');
-  const closebtn = document.querySelector('.popuplinks');
-  close.addEventListener('click', () => {
-    body.removeChild(main);
+      const close = document.querySelector('.close-button');
+      const closebtn = document.querySelector('.popuplinks');
+      close.addEventListener('click', () => {
+        body.removeChild(main);
+      });
+      closebtn.addEventListener('click', () => {
+        body.removeChild(main);
+      });
+    });
   });
-  closebtn.addEventListener('click', () => {
-    body.removeChild(main);
-  });
-});
-
-
-
- 
-    
-    })
 }
-popup()
+popup();
--- work.js
+++ work.js
@@ -1,96 +1,82 @@
 const workcard = [
-    {
-      id: 0,
-      name: "Multi-Post Stories",
-      description:
+  {
+    id: 0,
+    name: 'Multi-Post Stories',
+    description:
         "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
-      Fimage: "img/Img Placeholder.svg",
-      technologies: "img/Tags.svg",
-      Llive:"https://cluab.github.io/Portfolio/",
-      Lsource:"https://github.com/Cluab/Portfolio"
-    } ]
-    const workcardtwo = [
-    {
-        id: 1,
-        name: "Multi-Post Stories",
-        description:
+    Fimage: 'img/Img Placeholder.svg',
+    technologies: 'img/Tags.svg',
+    Llive: 'https://cluab.github.io/Portfolio/',
+    Lsource: 'https://github.com/Cluab/Portfolio',
+  }];
+const workcardtwo = [
+  {
+    id: 1,
+    name: 'Multi-Post Stories',
+    description:
           "A daily selection of privately personalized reads; no accounts  or sign-ups required. has been the industry's standard",
-        Fimage: "worksec worktwo cards",
-        technologies: ["html","bootstrap","Ruby"],
-        Llive:"https://cluab.github.io/Portfolio/",
-        Lsource:"https://github.com/Cluab/Portfolio"
-      },
-      {
-        id: 2,
-        name: "Data Dashboard Healthcare",
-        description:
+    Fimage: 'worksec worktwo cards',
+    technologies: ['html', 'bootstrap', 'Ruby'],
+    Llive: 'https://cluab.github.io/Portfolio/',
+    Lsource: 'https://github.com/Cluab/Portfolio',
+  },
+  {
+    id: 2,
+    name: 'Data Dashboard Healthcare',
+    description:
           "A daily selection of privately personalized reads; no accounts  or sign-ups required. has been the industry's standard",
-        Fimage: "workthree worktwo cards",
-        technologies: ["html","bootstrap","Ruby"],
-        Llive:"https://cluab.github.io/Portfolio/",
-        Lsource:"https://github.com/Cluab/Portfolio"
-      },
-      {
-        id: 3,
-        name: "Multi-Post Stories",
-        description:
+    Fimage: 'workthree worktwo cards',
+    technologies: ['html', 'bootstrap', 'Ruby'],
+    Llive: 'https://cluab.github.io/Portfolio/',
+    Lsource: 'https://github.com/Cluab/Portfolio',
+  },
+  {
+    id: 3,
+    name: 'Multi-Post Stories',
+    description:
           "A daily selection of privately personalized reads; no accounts  or sign-ups required. has been the industry's standard",
-        Fimage: "workfour worktwo cards",
-        technologies: ["html","bootstrap","Ruby"],
-        Llive:"https://cluab.github.io/Portfolio/",
-        Lsource:"https://github.com/Cluab/Portfolio"
-      },
-      {
-        id: 4,
-        name: "Multi-Post Stories",
-        description:
+    Fimage: 'workfour worktwo cards',
+    technologies: ['html', 'bootstrap', 'Ruby'],
+    Llive: 'https://cluab.github.io/Portfolio/',
+    Lsource: 'https://github.com/Cluab/Portfolio',
+  },
+  {
+    id: 4,
+    name: 'Multi-Post Stories',
+    description:
           "A daily selection of privately personalized reads; no accounts  or sign-ups required. has been the industry's standard",
-        Fimage: "worksec worktwo cards",
-        technologies: ["html","bootstrap","Ruby"],
-        Llive:"https://cluab.github.io/Portfolio/",
-        Lsource:"https://github.com/Cluab/Portfolio"
-      },
-      {
-        id: 5,
-        name: "Data Dashboard Healthcare",
-        description:
+    Fimage: 'worksec worktwo cards',
+    technologies: ['html', 'bootstrap', 'Ruby'],
+    Llive: 'https://cluab.github.io/Portfolio/',
+    Lsource: 'https://github.com/Cluab/Portfolio',
+  },
+  {
+    id: 5,
+    name: 'Data Dashboard Healthcare',
+    description:
           "A daily selection of privately personalized reads; no accounts  or sign-ups required. has been the industry's standard",
-        Fimage: "workthree worktwo cards",
-        technologies: ["html","bootstrap","Ruby"],
-        Llive:"https://cluab.github.io/Portfolio/",
-        Lsource:"https://github.com/Cluab/Portfolio"
-      },
-      {
-        id: 6,
-        name: "Multi-Post Stories",
-        description:
+    Fimage: 'workthree worktwo cards',
+    technologies: ['html', 'bootstrap', 'Ruby'],
+    Llive: 'https://cluab.github.io/Portfolio/',
+    Lsource: 'https://github.com/Cluab/Portfolio',
+  },
+  {
+    id: 6,
+    name: 'Multi-Post Stories',
+    description:
           "A daily selection of privately personalized reads; no accounts  or sign-ups required. has been the industry's standard",
-        Fimage: "workfour worktwo cards",
-        technologies: ["html","bootstrap","Ruby"],
-        Llive:"https://cluab.github.io/Portfolio/",
-        Lsource:"https://github.com/Cluab/Portfolio"
-      },
-    ]
+    Fimage: 'workfour worktwo cards',
+    technologies: ['html', 'bootstrap', 'Ruby'],
+    Llive: 'https://cluab.github.io/Portfolio/',
+    Lsource: 'https://github.com/Cluab/Portfolio',
+  },
+];
 
-    const workcardpop = [
-        {
-          id: 0,
-          name: ["Multi-Post Stories","Keeping track of hundreds  of components website"],
-          description:
-           ["Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,but also the leap into electronic typesetting, remaining essent",
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita."],
-          Fimage: "img/Snapshoot Portfolio.png",
-          technologies: ["html","bootstrap","Ruby"],
-          Llive:"https://cluab.github.io/Portfolio/",
-          Lsource:"https://github.com/Cluab/Portfolio"
-        } ]
+const workcards = document.querySelector('.rwboxbig');
 
-    const workcards = document.querySelector(".rwboxbig")
-
-
-function renderworkcards(){
-    workcard.forEach( (workcard) => {
-        workcards.innerHTML += `
+function renderworkcards() {
+  workcard.forEach((workcard) => {
+    workcards.innerHTML += `
       
     <div class="RWbox"><h3 class="RWork">"My Recent works"</h3>
        <div class="workline"></div>
@@ -101,15 +87,15 @@ function renderworkcards(){
          <img class="languages-Fwork" src="${workcard.technologies}" alt="languages used at recent work (css,html,bootstrap,ruby)">
         <button class="see-project" id="see">see project</button>
       </div> </div>
-    `
-    })
+    `;
+  });
 }
-renderworkcards()
+renderworkcards();
 
-const workcardgrid = document.querySelector(".diffrent-work")
-function renderworkcardtwo(){
-    workcardtwo.forEach( (workcardtwo) => {
-        workcardgrid.innerHTML += `
+const workcardgrid = document.querySelector('.diffrent-work');
+function renderworkcardtwo() {
+  workcardtwo.forEach((workcardtwo) => {
+    workcardgrid.innerHTML += `
         <div class="${workcardtwo.Fimage}">
           <div class="work-box-three work-box-two">
             <h5 class="work-three-title work-two-title">${workcardtwo.name}</h5>
@@ -122,9 +108,7 @@ function renderworkcardtwo(){
             </div>
             <button class="see-two see-two-box see-three-box show" id="see">see Project</button>
            </div>
-    `
-    
-    })
+    `;
+  });
 }
-renderworkcardtwo()
-
+renderworkcardtwo();
