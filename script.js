60390f3fa2a4a7aeef0637536b27478910a2b322
cluab <cluab11222@gmail.com>
2022-11-09 10:36:52 +0200

    added firstt project to portfolio

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.ul-nav');
 const logo = document.querySelector('.name');
 const bars = document.querySelector('.bars');
-const project = document.querySelector('.work-section');
+const project = document.querySelector('.see-project');
 const body = document.querySelector('body');
+const projects = document.querySelector('.see-two');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
@@ -12,27 +13,16 @@ hamburger.addEventListener('click', () => {
   bars.classList.toggle('active');
 });
 
-document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
-  hamburger.classList.remove('active');
-  navMenu.classList.remove('active');
-  logo.classList.remove('active');
-}));
-
-const workcardpop = [
-  {
-    id: 0,
-    name: ['Multi-Post Stories', 'Keeping track of hundreds  of components website'],
-    description:
-           ['Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,but also the leap into electronic typesetting, remaining essent',
-             "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita."],
-    Fimage: 'img/Snapshoot Portfolio.png',
-    technologies: ['html', 'bootstrap', 'Ruby'],
-    Llive: 'https://cluab.github.io/Portfolio/',
-    Lsource: 'https://github.com/Cluab/Portfolio',
-  }];
+document.querySelectorAll('.nav-link').forEach((n) =>
+  n.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+    logo.classList.remove('active');
+  })
+);
 
 function popup() {
-  workcardpop.forEach((workcardpop) => {
+  workcard.forEach((workcard) => {
     project.addEventListener('click', () => {
       const main = document.createElement('div');
       main.className = 'main-popup';
@@ -43,30 +33,29 @@ function popup() {
 <div class="popup-menu">
   <div class="popgap">
         <div class="popup-header">
-           <h2 class="popup-title">${workcardpop.name[0]}</h2>
-           <h2 class="popup-title-2">${workcardpop.name[1]}</h2>
+           <h2 class="popup-title">${workcard.name}</h2>
+           <h2 class="popup-title-2">${workcard.name}</h2>
            <button class="close-button">&times;</button>
         </div>
         <div class="popup-languages">
         <ul class="languages-work">
-                <li class=" popup-lan-box"><p class="code-name pop-name">${workcardpop.technologies[0]}</li>
-                <li class=" popup-lan-box"><p class="code-name pop-name">${workcardpop.technologies[1]}</p></li>
-                <li class=" popup-lan-box"><p class="code-name pop-name">${workcardpop.technologies[2]}</p></li>
+                <li class=" popup-lan-box"><p class="code-name pop-name">${workcard.technologies[1]}</li>
+                <li class=" popup-lan-box"><p class="code-name pop-name">${workcard.technologies[2]}</p></li>
+                <li class=" popup-lan-box"><p class="code-name pop-name">${workcard.technologies[3]}</p></li>
         </ul>
  </div>
    </div>
    <div class ="desktop-popup">
-   <img class='popup-image' src="img/Snapshoot Portfolio.png" alt="popup work preview">
+   <img class='popup-image' src="${workcard.Fimage}" alt="popup work preview">
    <div class="popup-end">
-     <p class="lorem">${workcardpop.description[0]}</p>
-   <p class="lorem-1">${workcardpop.description[1]}</p>
-    <div class="popuplinks"><a href="${workcardpop.Llive}">
+     <p class="lorem">${workcard.description}</p>
+   <p class="lorem-1">${workcard.description}</p>
+    <div class="popuplinks"><a href="${workcard.Llive}">
     <button class="live">See Live<img src="img/Icon.png" alt="icon-image"></button></a>
-    <a href="${workcardpop.Lsource}">
+    <a href="${workcard.Lsource}">
     <button class="live">See Source<img src="img/Vector.png" alt="github-icon"></button></a></div>
     </div>
-    </div></div>      
-`;
+    </div></div>`;
 
       main.appendChild(popup);
       body.appendChild(main);
@@ -83,3 +72,56 @@ function popup() {
   });
 }
 popup();
+
+function works() {
+  workcardtwo.forEach((workcardtwo) => {
+    projects.addEventListener('click', () => {
+      // if (projects.id === workcardtwo.id ){
+      const main = document.createElement('div');
+      main.className = 'main-popup';
+      const popup = document.createElement('div');
+      popup.className = 'popup';
+
+      popup.innerHTML = `
+<div class="popup-menu">
+  <div class="popgap">
+        <div class="popup-header">
+           <h2 class="popup-title">${workcardtwo.name}</h2>
+           <h2 class="popup-title-2">${workcardtwo.name}</h2>
+           <button class="close-button">&times;</button>
+        </div>
+        <div class="popup-languages">
+        <ul class="languages-work">
+                <li class=" popup-lan-box"><p class="code-name pop-name">${workcardtwo.technologies[1]}</li>
+                <li class=" popup-lan-box"><p class="code-name pop-name">${workcardtwo.technologies[2]}</p></li>
+                <li class=" popup-lan-box"><p class="code-name pop-name">${workcardtwo.technologies[3]}</p></li>
+        </ul>
+ </div>
+   </div>
+   <div class ="desktop-popup">
+   <img class='popup-image' src="${workcardtwo.Fimage}" alt="popup work preview">
+   <div class="popup-end">
+     <p class="lorem">${workcardtwo.description}</p>
+   <p class="lorem-1">${workcardtwo.description}</p>
+    <div class="popuplinks"><a href="${workcardtwo.Llive}">
+    <button class="live">See Live<img src="img/Icon.png" alt="icon-image"></button></a>
+    <a href="${workcardtwo.Lsource}">
+    <button class="live">See Source<img src="img/Vector.png" alt="github-icon"></button></a></div>
+    </div>
+    </div></div>`;
+
+      main.appendChild(popup);
+      body.appendChild(main);
+
+      const close = document.querySelector('.close-button');
+      const closebtn = document.querySelector('.popuplinks');
+      close.addEventListener('click', () => {
+        body.removeChild(main);
+      });
+      closebtn.addEventListener('click', () => {
+        body.removeChild(main);
+      });
+    });
+  });
+}
+works();
--- work.js
+++ work.js
@@ -1,21 +1,22 @@
 const workcard = [
   {
     id: 0,
-    name: 'Multi-Post Stories',
+    name: 'Microverse summary',
     description:
-        "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
-    Fimage: 'img/Img Placeholder.svg',
-    technologies: 'img/Tags.svg',
-    Llive: 'https://cluab.github.io/Portfolio/',
-    Lsource: 'https://github.com/Cluab/Portfolio',
-  }];
+      'this work is done a first capstone project that ive done at Microverse its to create an article that chose a topic i chose to talk about Microverse school and where i created a page useing pure HTML, CSS and some javascript',
+    Fimage: 'img/Pasted image.png',
+    technologies: ['img/Tags.svg', 'HTML', 'CSS', 'JAVASCRIPT'],
+    Llive: 'https://cluab.github.io/portal-capstone1/index.html',
+    Lsource: 'https://github.com/Cluab/portal-capstone1',
+  },
+];
 const workcardtwo = [
   {
     id: 1,
     name: 'Multi-Post Stories',
     description:
-          "A daily selection of privately personalized reads; no accounts  or sign-ups required. has been the industry's standard",
-    Fimage: 'worksec worktwo cards',
+      "A daily selection of privately personalized reads; no accounts  or sign-ups required. has been the industry's standard",
+    Fimage: ['worksec worktwo cards', 'img/Snapshoot Portfolio.png'],
     technologies: ['html', 'bootstrap', 'Ruby'],
     Llive: 'https://cluab.github.io/Portfolio/',
     Lsource: 'https://github.com/Cluab/Portfolio',
@@ -24,8 +25,8 @@ const workcardtwo = [
     id: 2,
     name: 'Data Dashboard Healthcare',
     description:
-          "A daily selection of privately personalized reads; no accounts  or sign-ups required. has been the industry's standard",
-    Fimage: 'workthree worktwo cards',
+      "A daily selection of privately personalized reads; no accounts  or sign-ups required. has been the industry's standard",
+    Fimage: ['worksec worktwo cards', 'img/Snapshoot Portfolio.png'],
     technologies: ['html', 'bootstrap', 'Ruby'],
     Llive: 'https://cluab.github.io/Portfolio/',
     Lsource: 'https://github.com/Cluab/Portfolio',
@@ -34,8 +35,8 @@ const workcardtwo = [
     id: 3,
     name: 'Multi-Post Stories',
     description:
-          "A daily selection of privately personalized reads; no accounts  or sign-ups required. has been the industry's standard",
-    Fimage: 'workfour worktwo cards',
+      "A daily selection of privately personalized reads; no accounts  or sign-ups required. has been the industry's standard",
+    Fimage: ['worksec worktwo cards', 'img/Snapshoot Portfolio.png'],
     technologies: ['html', 'bootstrap', 'Ruby'],
     Llive: 'https://cluab.github.io/Portfolio/',
     Lsource: 'https://github.com/Cluab/Portfolio',
@@ -44,8 +45,8 @@ const workcardtwo = [
     id: 4,
     name: 'Multi-Post Stories',
     description:
-          "A daily selection of privately personalized reads; no accounts  or sign-ups required. has been the industry's standard",
-    Fimage: 'worksec worktwo cards',
+      "A daily selection of privately personalized reads; no accounts  or sign-ups required. has been the industry's standard",
+    Fimage: ['worksec worktwo cards', 'img/Snapshoot Portfolio.png'],
     technologies: ['html', 'bootstrap', 'Ruby'],
     Llive: 'https://cluab.github.io/Portfolio/',
     Lsource: 'https://github.com/Cluab/Portfolio',
@@ -54,8 +55,8 @@ const workcardtwo = [
     id: 5,
     name: 'Data Dashboard Healthcare',
     description:
-          "A daily selection of privately personalized reads; no accounts  or sign-ups required. has been the industry's standard",
-    Fimage: 'workthree worktwo cards',
+      "A daily selection of privately personalized reads; no accounts  or sign-ups required. has been the industry's standard",
+    Fimage: ['worksec worktwo cards', 'img/Snapshoot Portfolio.png'],
     technologies: ['html', 'bootstrap', 'Ruby'],
     Llive: 'https://cluab.github.io/Portfolio/',
     Lsource: 'https://github.com/Cluab/Portfolio',
@@ -64,8 +65,8 @@ const workcardtwo = [
     id: 6,
     name: 'Multi-Post Stories',
     description:
-          "A daily selection of privately personalized reads; no accounts  or sign-ups required. has been the industry's standard",
-    Fimage: 'workfour worktwo cards',
+      "A daily selection of privately personalized reads; no accounts  or sign-ups required. has been the industry's standard",
+    Fimage: ['worksec worktwo cards', 'img/Snapshoot Portfolio.png'],
     technologies: ['html', 'bootstrap', 'Ruby'],
     Llive: 'https://cluab.github.io/Portfolio/',
     Lsource: 'https://github.com/Cluab/Portfolio',
@@ -84,8 +85,8 @@ function renderworkcards() {
       <div class="workone"><img class="workphoto"src="${workcard.Fimage}" alt="${workcard.name}">
         <div class="work-dis"><h4 class="first-Rwork">${workcard.name}</h4>
          <p class="work-dicreption">${workcard.description}.</p>
-         <img class="languages-Fwork" src="${workcard.technologies}" alt="languages used at recent work (css,html,bootstrap,ruby)">
-        <button class="see-project" id="see">see project</button>
+         <img class="languages-Fwork" src="${workcard.technologies[0]}" alt="languages used at recent work (css,html,bootstrap,ruby)">
+        <button class="see-project " id="${workcard.id}">see project</button>
       </div> </div>
     `;
   });
@@ -96,7 +97,7 @@ const workcardgrid = document.querySelector('.diffrent-work');
 function renderworkcardtwo() {
   workcardtwo.forEach((workcardtwo) => {
     workcardgrid.innerHTML += `
-        <div class="${workcardtwo.Fimage}">
+        <div class="${workcardtwo.Fimage[0]}">
           <div class="work-box-three work-box-two">
             <h5 class="work-three-title work-two-title">${workcardtwo.name}</h5>
             <p class="work-three-dis work-two-dis">${workcardtwo.description}</p>
@@ -106,7 +107,7 @@ function renderworkcardtwo() {
                 <li class="ruby-box"><p class="code-name">${workcardtwo.technologies[2]}</p></li>
               </ul>
             </div>
-            <button class="see-two see-two-box see-three-box show" id="see">see Project</button>
+            <button class="see-two see-two-box see-three-box show some-pop" id="${workcardtwo.id}">see Project</button>
            </div>
     `;
   });
